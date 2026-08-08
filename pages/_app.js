a9bd933eb7d460caa0379ad01ebc6de862584637
ZADDY ZAD <zubair428kg@gmail.com>
2026-08-08 14:17:23 +0530

    Load streamlined professional wallet theme

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "@rainbow-me/rainbowkit/styles.css";
 import AppErrorBoundary from "../components/app-error-boundary";
 import AppProviders from "../components/app-providers";
 import "../styles/globals.css";
-import "../styles/community.css";
+import "../styles/wallet-pro.css";
 
 export default function App({ Component, pageProps }) {
   return (
