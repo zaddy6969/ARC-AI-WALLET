edd1720719e7628234f6713cc561c8d8495e7bfd
ZADDY ZAD <zubair428kg@gmail.com>
2026-08-08 13:47:21 +0530

    Load Arc community suite styles

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "@rainbow-me/rainbowkit/styles.css";
 import AppErrorBoundary from "../components/app-error-boundary";
 import AppProviders from "../components/app-providers";
 import "../styles/globals.css";
+import "../styles/community.css";
 
 export default function App({ Component, pageProps }) {
   return (
