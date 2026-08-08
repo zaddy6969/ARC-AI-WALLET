9e84c162eea927fd350e76609e0994b2e8833a8a
ZADDY ZAD <zubair428kg@gmail.com>
2026-08-08 15:01:44 +0530

    Load clean professional wallet theme

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import AppErrorBoundary from "../components/app-error-boundary";
 import AppProviders from "../components/app-providers";
 import "../styles/globals.css";
 import "../styles/wallet-pro.css";
+import "../styles/wallet-clean.css";
 
 export default function App({ Component, pageProps }) {
   return (
