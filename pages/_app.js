c41864973e8c769e3d716a5467a0a672411ac274
ZADDY ZAD <zubair428kg@gmail.com>
2026-09-04 15:03:14 +0530

    Make Send, Swap and Bridge theme-aware
    Apply the existing persistent wallet light/dark toggle to all Send, Swap and Bridge surfaces without changing transaction logic.

--- pages/_app.js
+++ pages/_app.js
@@ -24,6 +24,7 @@ import "../styles/wallet-v3-bridge.css";
 import "../styles/wallet-v4.css";
 import "../styles/wallet-shell-theme.css";
 import "../styles/wallet-layout-final.css";
+import "../styles/wallet-action-theme.css";
 
 export default function App({ Component, pageProps }) {
   return (
